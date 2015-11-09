8f3f0f39e70076735d18221e04c12b95b85393ef
vrobles <vanessita.robles@gmail.com>
2015-11-08 21:02:03 -0500

    Se creó la función para cargar la fecha del sistema, y se actualizo la función de la hora para que se actualice minuto a minuto.

--- app/src/main/java/com/example/robles/mobi/alarma_noticias/MainActivity.java
+++ app/src/main/java/com/example/robles/mobi/alarma_noticias/MainActivity.java
@@ -1,30 +1,227 @@
 package com.example.robles.mobi.alarma_noticias;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.content.Intent;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.res.Resources;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.TabHost;
+import android.widget.TabHost.OnTabChangeListener;
+import android.widget.TextView;
+import java.util.Calendar;
+import android.widget.Toast;
 
+public class MainActivity extends Activity {
 
-public class MainActivity extends ActionBarActivity {
+    TextView tHora;
+    int hora=0, minuto =0, segundo = 0;
+    Intent i;
+    Thread iniReloj = null;
+    Runnable r;
+    boolean isUpdate = false;
+    String sec, min, hor;
+
+    String curTime;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
+        tHora = (TextView) findViewById(R.id.tClock);
+
+        r = new RefreshClock();
+        iniReloj= new Thread(r);
+        iniReloj.start();
+
+        Resources res = getResources();
+
+        TabHost tabs=(TabHost)findViewById(android.R.id.tabhost);
+        tabs.setup();
+
+        TabHost.TabSpec spec=tabs.newTabSpec("mitab1"); //estamos creando una nueva especificacion
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("ALARMA", res.getDrawable(android.R.drawable.ic_btn_speak_now));
+        tabs.addTab(spec);
+
+        spec=tabs.newTabSpec("mitab2");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("LISTA DE ALARMAS",
+                res.getDrawable(android.R.drawable.ic_dialog_map));
+        tabs.addTab(spec);
+
+        spec=tabs.newTabSpec("mitab3");
+        spec.setContent(R.id.tab3);
+        spec.setIndicator("CONFIGURACION",
+                res.getDrawable(android.R.drawable.ic_dialog_map));
+        tabs.addTab(spec);
+
+        tabs.setCurrentTab(0);
+
+        tabs.setOnTabChangedListener(new OnTabChangeListener() {
+            public void onTabChanged(String tabId) {
+                Log.i("AndroidTabsDemo", "Pulsada pesta�a: " + tabId);
+
+
+
+            }
+        });
+
+
+        final TextView fecha_actual = (TextView) findViewById(R.id.fecha01);
+
+        final Calendar c = Calendar.getInstance();
+        int anio = c.get(Calendar.YEAR); //obtenemos el año
+        int mes = c.get(Calendar.MONTH); //obtenemos el mes
+
+        //Los meses se presentan del 0 al 11, representando el cero a Enero y el once a diciembre, por lo cual para su presentación
+
+        //sumaremos 1 a la variable entera MES.
+        mes = mes + 1;
+        int dia = c.get(Calendar.DAY_OF_MONTH); // obtemos el día.
+        fecha_actual.setText(dia+" : "+mes+" : "+anio); //cambiamos el texto que tiene el TextView por la fecha actual.
+
+    }
+
+
+    public void ajuste(View v){
+        i = new Intent(this, Ajustes.class);
+        i.putExtra(S.FULL_TIME, curTime);
+
+        // hay una clase llamada S, que se encarga de contener var estaticas como AJUSTES
+        this.startActivityForResult(i, S.AJUSTES);
+    }
+
+
+    /**
+     basicamente es el que hace el limpiado del layout cada segundo, un simple if es el
+     que identifica si se ah actualizado la hora desde ajustes oh si tiene que seguir
+     mostrando la hora actual
+
+     isUpdate muestra el valor que se envio de la  clase Ajustes
+     */
+    private void initClock() {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                try{
+
+                    if(isUpdate){
+                        settingNewClock();
+                    } else {
+                        updateTime();
+                    }
+                    curTime =hor+ hora + min + minuto + sec + segundo;
+                    tHora.setText(curTime);
+
+                }catch (Exception e) {}
+            }
+        });
+    }
+
+
+
+    /*
+     Esta clase hace uso de la interface Runnable la cual es la encargada de estar
+     refrescando cada 1000 milisegundos es decir, un segudo, no tiene gran ciencia
+
+
+     @SuppressWarning("unused")
+     es para decirle al compilador que obvie la advertencia
+     que se genera, pero la verdad no afecta en nada el funcionamiento del mismo
+
+     */
+    class RefreshClock implements Runnable{
+        // @Override
+        @SuppressWarnings("unused")
+        public void run() {
+            while(!Thread.currentThread().isInterrupted()){
+                try {
+                    initClock();
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }catch(Exception e){
+                }
+            }
         }
     }
 
+    /**
+     Este es el metodo inicial del reloj, a partir de el es que se muestra la hora
+     cada segundo es la encargada Java.Util.Calendar
+
+     */
+
+    private void updateTime(){
+
+        Calendar c = Calendar.getInstance();
+        hora = c.get(Calendar.HOUR_OF_DAY);
+        minuto = c.get(Calendar.MINUTE);
+        segundo = c.get(Calendar.SECOND);
+        setZeroClock();
+
+    }
+    /**
+     setZeroClock es para que se nos ponga el numero 0 en aquellos valores menores a
+     10, pero no he podido resolver un pequeño inconveniente al momento de la llegada
+     de 0:0:0 y por ende en sus derivadas, aunque no es por falta de logica, he revisado
+     muy bien, pero si le encuentran arreglo me hacen el favor y me avisan de como
+     solucionarlo
+     */
+    private void setZeroClock(){
+        if(hora >=0 & hora <=9){
+            hor = "0";
+        }else{
+            hor = "";
+        }
+
+        if(minuto >=0 & minuto <=9){
+            min = ":0";
+        }else{
+            min = ":";
+        }
+
+        if(segundo >=0 & segundo <=9){
+            sec = ":0";
+
+        }else{
+            sec = ":";
+        }
+    }
+
+
+    /**
+     Que puedo decir de este metodo mas que es el encargado de parsear la hora de una
+     manera que al llegar a 24:59:59 esta retome los valores de 00:00:00 aunque en la practica
+     como mencionaba en un comentario anterior esta se pone en 0:0:0, pero luego se restaura a
+     00:00:01
+     */
+    private void settingNewClock(){
+        segundo +=1;
+
+        setZeroClock();
+
+        if(segundo >=0 & segundo <=59){
+
+        }else {
+            segundo = 0;
+            minuto +=1;
+        }
+        if(minuto >=0 & minuto <=59){
+
+        }else{
+            minuto = 0;
+            hora +=1;
+        }
+        if(hora >= 0 & hora <= 24){
+
+        }else{
+            hora = 0;
+        }
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -34,33 +231,59 @@ public class MainActivity extends ActionBarActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == S.AJUSTES){
+            hora = data.getExtras().getInt(S.HORA);
+            minuto = data.getExtras().getInt(S.MINUTO);
+            segundo = data.getExtras().getInt(S.SEGUNDO);
+            isUpdate = data.getExtras().getBoolean(S.IS_UPDATE_TIME);
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
 
     /**
-     * A placeholder fragment containing a simple view.
+     Bueno el metodo mensaje se uso simplemente para pruebas y como se nota es un simple Toast
+
+     Tiene dos parametros, el mensaje que se desea mostrar y el tiempo que este dura
+     1 para un mensaje corto y cualquier otro valor  mayor a uno para mensajes largos
      */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
+    private void mensaje(String m, int i){
+        if(i==1){
+            Toast.makeText(getApplicationContext(), m, Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(getApplicationContext(), m, Toast.LENGTH_LONG).show();
         }
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public void onBackPressed() {
+        // TODO Auto-generated method stub
+        super.onBackPressed();
+        this.finish();
+    }
+
+
+
+
 }
---
+++ app/src/main/java/com/example/robles/mobi/alarma_noticias/S.java
@@ -0,0 +1,10 @@
+package com.example.robles.mobi.alarma_noticias;
+
+public class S {
+	public static int AJUSTES = 1;
+	public static String HORA = "hora";
+	public static String MINUTO = "minuto";
+	public static String SEGUNDO = "segundo";
+	public static String IS_UPDATE_TIME = "fue actualizaso";
+	public static String FULL_TIME = "la hora completa";
+}
