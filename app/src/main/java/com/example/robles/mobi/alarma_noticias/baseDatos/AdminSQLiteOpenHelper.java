9d2bbc0792ce8b30926980c45a09304858a3a274
lorena <lorpabar@espol.edu.ec>
2015-12-31 09:15:59 -0500

    creacion tabla mail

---
+++ app/src/main/java/com/example/robles/mobi/alarma_noticias/baseDatos/AdminSQLiteOpenHelper.java
@@ -0,0 +1,26 @@
+package com.example.robles.mobi.alarma_noticias.baseDatos;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
+
+
+    public AdminSQLiteOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL("create table mail(idMail int primary key NOT NULL AUTO_INCREMENT, nombre text, correo text, password text  )");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
