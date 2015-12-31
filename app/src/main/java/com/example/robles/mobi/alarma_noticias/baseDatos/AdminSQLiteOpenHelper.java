6a7f684024341e06870d6778619bf6ef23c94df7
lorena <lorpabar@espol.edu.ec>
2015-12-31 09:33:31 -0500

    creacion tabla noticias

--- app/src/main/java/com/example/robles/mobi/alarma_noticias/baseDatos/AdminSQLiteOpenHelper.java
+++ app/src/main/java/com/example/robles/mobi/alarma_noticias/baseDatos/AdminSQLiteOpenHelper.java
@@ -18,6 +18,9 @@ public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
         db.execSQL("create table mail(idMail int primary key NOT NULL AUTO_INCREMENT, nombre text, correo text, password text  )");
         db.execSQL("create table noticias(idNoticias int primary key NOT NULL AUTO_INCREMENT, nombre text, urlNoticias text  )");
         db.execSQL("create table radio(idRadio int primary key NOT NULL AUTO_INCREMENT, nombre text, urlRadio text  )");
+        db.execSQL("create table alarma(idAlarma int primary key NOT NULL AUTO_INCREMENT, hora text, fecha text, etiqueta text, idNoticias int, " +
+                "idNoticias int FOREIGN KEY REFERENCES noticias(idNoticias), idRadio int, idRadio int FOREIGN KEY REFERENCES radio(idRadio)" +
+                "idMail int,  idMail int FOREIGN KEY REFERENCES mail(idMail) )");
 
     }
 
